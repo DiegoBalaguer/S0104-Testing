78189b92dd07c9feb78941ac517a27a7562eae39
Diego.Balaguer <didacb@gmail.com>
2025-05-01 23:48:40 +0200

    test(level03): add test exercise 01 - write initial TDD tests for Calculator class

---
+++ src/test/java/level03/exercise01/CalculatorTest.java
@@ -0,0 +1,101 @@
+package level03.exercise01;
+
+import level03.exercise01.model.Calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * PROGRAM: CalculatorTest
+ * AUTHOR: Diego Balaguer
+ * DATE: 20/04/2025
+ */
+
+public class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("additionProvider")
+    public void givenTwoNumbers_whenAdd_thenReturnsCorrectSum(double operator01, double operator02, double expected) {
+        assertEquals(expected, Calculator.add(operator01, operator02));
+    }
+
+    static Stream<Arguments> additionProvider() {
+        return Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of(12, 8, 20),
+                org.junit.jupiter.params.provider.Arguments.of(20, 0, 20),
+                org.junit.jupiter.params.provider.Arguments.of(-10, 5, -5),
+                org.junit.jupiter.params.provider.Arguments.of(0, 0, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("subtractionProvider")
+    public void givenTwoNumbers_whenSubtract_thenReturnsCorrectDifference(double operator01, double operator02, double expected) {
+        assertEquals(expected, Calculator.subtract(operator01, operator02));
+    }
+
+    static Stream<org.junit.jupiter.params.provider.Arguments> subtractionProvider() {
+        return Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of(12, 8, 4),
+                org.junit.jupiter.params.provider.Arguments.of(20, 0, 20),
+                org.junit.jupiter.params.provider.Arguments.of(-10, 5, -15),
+                org.junit.jupiter.params.provider.Arguments.of(0, 0, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("multiplicationProvider")
+    public void givenTwoNumbers_whenMultiply_thenReturnsCorrectProduct(double operator01, double operator02, double expected) {
+        assertEquals(expected, Calculator.multiply(operator01, operator02));
+    }
+
+    static Stream<org.junit.jupiter.params.provider.Arguments> multiplicationProvider() {
+        return Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of(6, 6, 36),
+                org.junit.jupiter.params.provider.Arguments.of(3, 4, 12),
+                org.junit.jupiter.params.provider.Arguments.of(-4, 5, -20),
+                org.junit.jupiter.params.provider.Arguments.of(10, 0, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("divisionProvider")
+    public void givenTwoNumbers_whenDivide_thenReturnsCorrectQuotient(double operator01, double operator02, double expected) {
+        assertEquals(expected, Calculator.divide(operator01, operator02));
+    }
+
+    static Stream<org.junit.jupiter.params.provider.Arguments> divisionProvider() {
+        return Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of(12, 4, 3),
+                org.junit.jupiter.params.provider.Arguments.of(20, 5, 4),
+                org.junit.jupiter.params.provider.Arguments.of(-10, 5, -2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("divisionByZeroProvider")
+    public void givenDivideByZero_whenDivide_thenThrowsArithmeticException(double operator01, double operator02) {
+        assertThatThrownBy(() -> Calculator.divide(operator01, operator02))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessageContaining("Divide by zero");
+    }
+
+    static Stream<org.junit.jupiter.params.provider.Arguments> divisionByZeroProvider() {
+        return Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of(5, 0),
+                org.junit.jupiter.params.provider.Arguments.of(-3, 0)
+        );
+    }
+}
